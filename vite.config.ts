69f334e77f02f0e173fec509d43bf198b6839194
Elson Liang <elsonliang@gmail.com>
2023-07-26 01:38:16 -0700

    Expose env variables

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dotenv from 'dotenv'
 
+// expose environmental variables to netlify
+const viteEnv: { [key: string]: string | undefined } = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: viteEnv,
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
